5a3f256bf54198bc4672d19c3668f0ca39b27254
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-13 07:23:55 +0000

    [Finish] 4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,6 +4,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def create
@@ -14,17 +15,20 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
+
   def destroy
-    
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
-  
+
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # get 'post_images/index'
   # get 'post_images/show'
   
-   resources :post_images, only: [:new, :create, :index, :show]
+   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get "/homes/about" => "homes#about"
+  
 end
