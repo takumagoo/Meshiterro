71d3e1f251c3b239cf65ffc2a0b13f1aa58433a1
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-13 04:12:09 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get 'post_images/index'
   # get 'post_images/show'
   
-   resources :post_images, only: [:new, :index, :show]
+   resources :post_images, only: [:new, :create, :index, :show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
