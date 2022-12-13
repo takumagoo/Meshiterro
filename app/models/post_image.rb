3ad366bd3f6fd01c38ec871858834784fe2c943c
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-13 03:08:43 +0000

    [Finish] 4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,6 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
+  
+  # PostImage モデルに User モデルを関連付ける
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  devise_for :users
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  
+   resources :post_images, only: [:new, :index, :show]
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   get "/homes/about" => "homes#about"
 end
