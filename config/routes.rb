7fe111f0e471cdb351e2ab8a6882c233b63b3659
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-15 06:08:36 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,18 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  
+
   # resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :edit, :update]
-  
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   get "/homes/about" => "homes#about"
-  
+
 end
