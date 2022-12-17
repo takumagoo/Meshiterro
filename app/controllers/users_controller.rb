2a342b494f216962e196584f8d64bcfbf39641a1
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-17 13:08:49 +0000

    [Finish] 4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     # @post_images = @user.post_images
@@ -6,10 +9,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # is_matching_login_user
+
+    # user_id = params[:id].to_i
+    # login_user_id = current_user.id
+    # if(user_id != login_user_id)
+    #   redirect_to post_images_path
+    # end
     @user = User.find(params[:id])
   end
 
   def update
+    # is_matching_login_user
+
+    # user_id = params[:id].to_i
+    # login_user_id = current_user.id
+    # if(user_id != login_user_id)
+    #   redirect_to post_images_path
+    # end
     @user = User.find(params[:id])
     @user.update(user_params)
 
@@ -23,4 +40,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
+  end
 end
